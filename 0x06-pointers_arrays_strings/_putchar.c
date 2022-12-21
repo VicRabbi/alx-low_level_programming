5bf648862e092a5b8957a2e76cef9364a10e8359
VicRabbi <idokov90@yahoo.com>
2022-12-21 07:16:40 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+#include <unistd.h>
+
+typedef int make_iso_compilers_happy;
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to printReturn: On success 1.
+ * On error, -1 is returned, and errno is set appropriately
+ */
+
+int _putchar(char c)
+
+{
+	return (write(1, &c, 1));
+}
