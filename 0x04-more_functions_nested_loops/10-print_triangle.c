9e804d20783ff886f5ed1958a95478519df82a7f
VicRabbi <idokov90@yahoo.com>
2022-12-26 04:55:13 -0800

    a program that finds and prints the largest prime factor of the number

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,15 +17,15 @@ void print_triangle(int size)
 
 		for (i = 1; i <= size; i++)
 		{
-			for (j = i; j < size; j++)
+			for (j = 1; j < size; j++)
 			{
 				 _putchar(' ');
-			}
+			}	
 			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
 			}
-			_putchar('\n');
+		_putchar('\n');
 		}
 	}
 }
