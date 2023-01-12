c640d6d34ecae6b72f69ac00c7920a5f53fb50f1
VicRabbi <idokov90@yahoo.com>
2023-01-12 03:25:07 -0800

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,11 +9,11 @@ int factorial(int n)
 {
 if (n == 0) /*Base condition*/
 
-retrun (1);
+return (1);
 
 else if (n < 0) /*Base condition*/
 return (-1);
 else
-return (n * factorial(n - 1); /*Recursive call*/
+return (n * factorial(n - 1)); /*Recursive call*/
 
 }
